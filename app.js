3f96bcc41cb6763b1542cf0b7e744a6c37487be3
PiotrBerebecki <petebere@gmail.com>
2017-03-04 15:56:43 +0000

    Add window onload to prevent jest errors

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ function sum(a, b) {
   return a + b;
 }
 
-
 console.log('Not part of module.exports but still appearing in terminal, why?');
-var button = document.getElementById('button');
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-});
 
-module.exports = {
-  sum
+window.onload = function() {
+  var button = document.getElementById('button');
+  button.addEventListener('click', function(e) {
+    console.log('button was clicked');
+  });
 };
+
+
+  module.exports = {
+    sum
+  };
